bf3a75ec23b69d552217cd881caa496260bd6524
Fagghino <fgiordano839@gmail.com>
2026-05-09 20:32:56 +0200

    refactor(screen): update CobbleFarmScreenHandlers to Yarn mappings

--- src/main/java/com/cobblefarm/screen/CobbleFarmScreenHandlers.java
+++ src/main/java/com/cobblefarm/screen/CobbleFarmScreenHandlers.java
@@ -1,9 +1,9 @@
 package com.cobblefarm.screen;
 
 import com.cobblefarm.CobbleFarm;
-import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.screen.ScreenHandlerType;
 
 public class CobbleFarmScreenHandlers {
@@ -14,7 +14,7 @@ public class CobbleFarmScreenHandlers {
         POKEMON_FARM_SCREEN_HANDLER = Registry.register(
                 Registries.SCREEN_HANDLER,
                 CobbleFarm.id("pokemon_farm"),
-                new ScreenHandlerType<>(PokemonFarmScreenHandler::new)
+                new ScreenHandlerType<>(PokemonFarmScreenHandler::new, FeatureFlags.VANILLA_FEATURES)
         );
         CobbleFarm.LOGGER.info("Registered CobbleFarm screen handlers.");
     }
