e3abe1069ff721d1cc8629091d72d5648f36c08d
Fagghino <fgiordano839@gmail.com>
2026-05-09 19:08:00 +0200

    feat: add client-side mod entry point

---
+++ src/main/java/com/cobblefarm/CobbleFarmClient.java
@@ -0,0 +1,18 @@
+package com.cobblefarm;
+
+import com.cobblefarm.screen.CobbleFarmScreenHandlers;
+import com.cobblefarm.screen.PokemonFarmScreen;
+import com.cobblefarm.screen.PokemonFarmScreenHandler;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
+
+public class CobbleFarmClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        HandledScreens.register(CobbleFarmScreenHandlers.POKEMON_FARM_SCREEN_HANDLER,
+                PokemonFarmScreen::new);
+
+        CobbleFarm.LOGGER.info("CobbleFarm client initialized.");
+    }
+}
