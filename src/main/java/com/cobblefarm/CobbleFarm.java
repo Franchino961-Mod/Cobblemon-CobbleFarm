a21c9efa91c6cd2a527e65ef38331cba6000af30
Fagghino <fgiordano839@gmail.com>
2026-05-09 20:32:52 +0200

    refactor(main): update CobbleFarm to Yarn mappings

--- src/main/java/com/cobblefarm/CobbleFarm.java
+++ src/main/java/com/cobblefarm/CobbleFarm.java
@@ -5,12 +5,16 @@ import com.cobblefarm.blockentity.CobbleFarmBlockEntities;
 import com.cobblefarm.item.CobbleFarmItems;
 import com.cobblefarm.network.CobbleFarmNetworking;
 import com.cobblefarm.screen.CobbleFarmScreenHandlers;
+import com.cobblemon.mod.common.api.Priority;
+import com.cobblemon.mod.common.api.events.CobblemonEvents;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
@@ -22,7 +26,7 @@ public class CobbleFarm implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static Identifier id(String path) {
-        return new Identifier(MOD_ID, path);
+        return Identifier.of(MOD_ID, path);
     }
 
     @Override
@@ -33,7 +37,31 @@ public class CobbleFarm implements ModInitializer {
         CobbleFarmBlocks.register();
         CobbleFarmBlockEntities.register();
         CobbleFarmScreenHandlers.register();
+        CobbleFarmNetworking.registerPayloads();
         CobbleFarmNetworking.registerServerPackets();
+
+        // Handle Pokémon capture with Farm Ball
+        CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.NORMAL, event -> {
+            ServerPlayerEntity player = event.getPlayer();
+            Pokemon pokemon = event.getPokemon();
+            
+            String species = pokemon.getSpecies().getResourceIdentifier().toString();
+            String form = pokemon.getForm().getName();
+            int level = pokemon.getLevel();
+            boolean shiny = pokemon.getShiny();
+            String displayName = pokemon.getDisplayName(true).getString();
+            String lootTable = "cobblemon:entities/" + pokemon.getSpecies().getName().toLowerCase();
+
+            ItemStack capturedItem = com.cobblefarm.item.CapturedPokemonItem.create(
+                species, form, level, shiny, displayName, lootTable
+            );
+
+            if (!player.getInventory().insertStack(capturedItem)) {
+                player.dropItem(capturedItem, false);
+            }
+            
+            return kotlin.Unit.INSTANCE;
+        });
 
         // Register creative tab
         Registry.register(Registries.ITEM_GROUP, id("cobblefarm"), FabricItemGroup.builder()
