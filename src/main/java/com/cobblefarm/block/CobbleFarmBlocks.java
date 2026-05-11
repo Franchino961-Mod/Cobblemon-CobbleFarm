2a808e357f1a16a39ddddb85b6d1020f803d4732
Fagghino <fgiordano839@gmail.com>
2026-05-11 20:19:51 +0200

    feat: update CobbleFarmBlocks.java

--- src/main/java/com/cobblefarm/block/CobbleFarmBlocks.java
+++ src/main/java/com/cobblefarm/block/CobbleFarmBlocks.java
@@ -11,27 +11,27 @@ import net.minecraft.sound.BlockSoundGroup;
 
 public class CobbleFarmBlocks {
 
-    public static final PokemonFarmBlock POKEMON_FARM_IRON = register("pokemon_farm_iron",
-            new PokemonFarmBlock(FarmTier.IRON, AbstractBlock.Settings.copy(Blocks.IRON_BLOCK)
-                    .strength(3.0f, 6.0f).sounds(BlockSoundGroup.METAL).requiresTool()));
+    public static final CobbleFarmBlock COBBLEFARM_IRON = register("cobblefarm_iron",
+            new CobbleFarmBlock(FarmTier.IRON, AbstractBlock.Settings.copy(Blocks.IRON_BLOCK)
+                    .strength(3.0f, 6.0f).sounds(BlockSoundGroup.METAL).requiresTool().nonOpaque()));
 
-    public static final PokemonFarmBlock POKEMON_FARM_GOLD = register("pokemon_farm_gold",
-            new PokemonFarmBlock(FarmTier.GOLD, AbstractBlock.Settings.copy(Blocks.GOLD_BLOCK)
-                    .strength(3.0f, 6.0f).sounds(BlockSoundGroup.METAL).requiresTool()));
+    public static final CobbleFarmBlock COBBLEFARM_GOLD = register("cobblefarm_gold",
+            new CobbleFarmBlock(FarmTier.GOLD, AbstractBlock.Settings.copy(Blocks.GOLD_BLOCK)
+                    .strength(3.0f, 6.0f).sounds(BlockSoundGroup.METAL).requiresTool().nonOpaque()));
 
-    public static final PokemonFarmBlock POKEMON_FARM_DIAMOND = register("pokemon_farm_diamond",
-            new PokemonFarmBlock(FarmTier.DIAMOND, AbstractBlock.Settings.copy(Blocks.DIAMOND_BLOCK)
-                    .strength(4.0f, 8.0f).sounds(BlockSoundGroup.METAL).requiresTool()));
+    public static final CobbleFarmBlock COBBLEFARM_DIAMOND = register("cobblefarm_diamond",
+            new CobbleFarmBlock(FarmTier.DIAMOND, AbstractBlock.Settings.copy(Blocks.DIAMOND_BLOCK)
+                    .strength(4.0f, 8.0f).sounds(BlockSoundGroup.METAL).requiresTool().nonOpaque()));
 
-    public static final PokemonFarmBlock POKEMON_FARM_EMERALD = register("pokemon_farm_emerald",
-            new PokemonFarmBlock(FarmTier.EMERALD, AbstractBlock.Settings.copy(Blocks.EMERALD_BLOCK)
-                    .strength(4.0f, 8.0f).sounds(BlockSoundGroup.METAL).requiresTool()));
+    public static final CobbleFarmBlock COBBLEFARM_EMERALD = register("cobblefarm_emerald",
+            new CobbleFarmBlock(FarmTier.EMERALD, AbstractBlock.Settings.copy(Blocks.EMERALD_BLOCK)
+                    .strength(4.0f, 8.0f).sounds(BlockSoundGroup.METAL).requiresTool().nonOpaque()));
 
-    public static final PokemonFarmBlock POKEMON_FARM_NETHERITE = register("pokemon_farm_netherite",
-            new PokemonFarmBlock(FarmTier.NETHERITE, AbstractBlock.Settings.copy(Blocks.NETHERITE_BLOCK)
-                    .strength(5.0f, 1200.0f).sounds(BlockSoundGroup.NETHERITE).requiresTool()));
+    public static final CobbleFarmBlock COBBLEFARM_NETHERITE = register("cobblefarm_netherite",
+            new CobbleFarmBlock(FarmTier.NETHERITE, AbstractBlock.Settings.copy(Blocks.NETHERITE_BLOCK)
+                    .strength(5.0f, 1200.0f).sounds(BlockSoundGroup.NETHERITE).requiresTool().nonOpaque()));
 
-    private static <T extends PokemonFarmBlock> T register(String name, T block) {
+    private static <T extends CobbleFarmBlock> T register(String name, T block) {
         Registry.register(Registries.BLOCK, CobbleFarm.id(name), block);
         Registry.register(Registries.ITEM, CobbleFarm.id(name),
                 new BlockItem(block, new Item.Settings()));
