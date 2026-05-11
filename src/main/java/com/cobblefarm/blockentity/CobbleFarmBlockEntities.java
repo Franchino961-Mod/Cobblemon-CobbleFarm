0b92437340b228c76f916c02af86d3afa33266dd
Fagghino <fgiordano839@gmail.com>
2026-05-11 20:19:52 +0200

    feat: update CobbleFarmBlockEntities.java

--- src/main/java/com/cobblefarm/blockentity/CobbleFarmBlockEntities.java
+++ src/main/java/com/cobblefarm/blockentity/CobbleFarmBlockEntities.java
@@ -10,26 +10,26 @@ import net.minecraft.registry.Registry;
 
 public class CobbleFarmBlockEntities {
 
-    public static BlockEntityType<PokemonFarmBlockEntity> POKEMON_FARM;
+    public static BlockEntityType<CobbleFarmBlockEntity> COBBLEFARM;
 
     public static void register() {
-        POKEMON_FARM = Registry.register(
+        COBBLEFARM = Registry.register(
                 Registries.BLOCK_ENTITY_TYPE,
-                CobbleFarm.id("pokemon_farm"),
+                CobbleFarm.id("cobblefarm"),
                 FabricBlockEntityTypeBuilder.create(
                         (pos, state) -> {
                             FarmTier tier = FarmTier.IRON;
-                            if (state.isOf(CobbleFarmBlocks.POKEMON_FARM_GOLD)) tier = FarmTier.GOLD;
-                            else if (state.isOf(CobbleFarmBlocks.POKEMON_FARM_DIAMOND)) tier = FarmTier.DIAMOND;
-                            else if (state.isOf(CobbleFarmBlocks.POKEMON_FARM_EMERALD)) tier = FarmTier.EMERALD;
-                            else if (state.isOf(CobbleFarmBlocks.POKEMON_FARM_NETHERITE)) tier = FarmTier.NETHERITE;
-                            return new PokemonFarmBlockEntity(pos, state, tier);
+                            if (state.isOf(CobbleFarmBlocks.COBBLEFARM_GOLD)) tier = FarmTier.GOLD;
+                            else if (state.isOf(CobbleFarmBlocks.COBBLEFARM_DIAMOND)) tier = FarmTier.DIAMOND;
+                            else if (state.isOf(CobbleFarmBlocks.COBBLEFARM_EMERALD)) tier = FarmTier.EMERALD;
+                            else if (state.isOf(CobbleFarmBlocks.COBBLEFARM_NETHERITE)) tier = FarmTier.NETHERITE;
+                            return new CobbleFarmBlockEntity(pos, state, tier);
                         },
-                        CobbleFarmBlocks.POKEMON_FARM_IRON,
-                        CobbleFarmBlocks.POKEMON_FARM_GOLD,
-                        CobbleFarmBlocks.POKEMON_FARM_DIAMOND,
-                        CobbleFarmBlocks.POKEMON_FARM_EMERALD,
-                        CobbleFarmBlocks.POKEMON_FARM_NETHERITE
+                        CobbleFarmBlocks.COBBLEFARM_IRON,
+                        CobbleFarmBlocks.COBBLEFARM_GOLD,
+                        CobbleFarmBlocks.COBBLEFARM_DIAMOND,
+                        CobbleFarmBlocks.COBBLEFARM_EMERALD,
+                        CobbleFarmBlocks.COBBLEFARM_NETHERITE
                 ).build()
         );
 
