4839dd87bd05ebe34c693178e03d1bd7c6e95c94
Fagghino <fgiordano839@gmail.com>
2026-05-11 20:19:48 +0200

    feat: update CobbleFarm.java

--- src/main/java/com/cobblefarm/CobbleFarm.java
+++ src/main/java/com/cobblefarm/CobbleFarm.java
@@ -5,9 +5,7 @@ import com.cobblefarm.blockentity.CobbleFarmBlockEntities;
 import com.cobblefarm.item.CobbleFarmItems;
 import com.cobblefarm.network.CobbleFarmNetworking;
 import com.cobblefarm.screen.CobbleFarmScreenHandlers;
-import com.cobblemon.mod.common.api.Priority;
 import com.cobblemon.mod.common.api.events.CobblemonEvents;
-import com.cobblemon.mod.common.pokemon.Pokemon;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
@@ -40,41 +38,17 @@ public class CobbleFarm implements ModInitializer {
         CobbleFarmNetworking.registerPayloads();
         CobbleFarmNetworking.registerServerPackets();
 
-        // Handle Pokémon capture with Farm Ball
-        CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.NORMAL, event -> {
-            ServerPlayerEntity player = event.getPlayer();
-            Pokemon pokemon = event.getPokemon();
-            
-            String species = pokemon.getSpecies().getResourceIdentifier().toString();
-            String form = pokemon.getForm().getName();
-            int level = pokemon.getLevel();
-            boolean shiny = pokemon.getShiny();
-            String displayName = pokemon.getDisplayName(true).getString();
-            String lootTable = "cobblemon:entities/" + pokemon.getSpecies().getName().toLowerCase();
-
-            ItemStack capturedItem = com.cobblefarm.item.CapturedPokemonItem.create(
-                species, form, level, shiny, displayName, lootTable
-            );
-
-            if (!player.getInventory().insertStack(capturedItem)) {
-                player.dropItem(capturedItem, false);
-            }
-            
-            return kotlin.Unit.INSTANCE;
-        });
-
         // Register creative tab
         Registry.register(Registries.ITEM_GROUP, id("cobblefarm"), FabricItemGroup.builder()
                 .displayName(Text.translatable("itemgroup.cobblefarm"))
                 .icon(() -> new ItemStack(CobbleFarmItems.FARM_BALL))
                 .entries((context, entries) -> {
                     entries.add(CobbleFarmItems.FARM_BALL);
-                    entries.add(CobbleFarmItems.CAPTURED_POKEMON_ITEM);
-                    entries.add(CobbleFarmBlocks.POKEMON_FARM_IRON.asItem());
-                    entries.add(CobbleFarmBlocks.POKEMON_FARM_GOLD.asItem());
-                    entries.add(CobbleFarmBlocks.POKEMON_FARM_DIAMOND.asItem());
-                    entries.add(CobbleFarmBlocks.POKEMON_FARM_EMERALD.asItem());
-                    entries.add(CobbleFarmBlocks.POKEMON_FARM_NETHERITE.asItem());
+                    entries.add(CobbleFarmBlocks.COBBLEFARM_IRON.asItem());
+                    entries.add(CobbleFarmBlocks.COBBLEFARM_GOLD.asItem());
+                    entries.add(CobbleFarmBlocks.COBBLEFARM_DIAMOND.asItem());
+                    entries.add(CobbleFarmBlocks.COBBLEFARM_EMERALD.asItem());
+                    entries.add(CobbleFarmBlocks.COBBLEFARM_NETHERITE.asItem());
                 })
                 .build());
 
