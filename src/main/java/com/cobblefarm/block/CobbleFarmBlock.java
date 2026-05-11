5d709f85ac76f7050c6e832f975298927ab4993b
Fagghino <fgiordano839@gmail.com>
2026-05-11 20:19:50 +0200

    refactor: rename PokemonFarmBlock.java to CobbleFarmBlock.java

--- src/main/java/com/cobblefarm/block/CobbleFarmBlock.java
+++ src/main/java/com/cobblefarm/block/CobbleFarmBlock.java
@@ -1,6 +1,6 @@
 package com.cobblefarm.block;
 
-import com.cobblefarm.blockentity.PokemonFarmBlockEntity;
+import com.cobblefarm.blockentity.CobbleFarmBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
@@ -19,16 +19,19 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.BlockView;
+import net.minecraft.util.shape.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class PokemonFarmBlock extends BlockWithEntity {
+public class CobbleFarmBlock extends BlockWithEntity {
 
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
-    public static final MapCodec<PokemonFarmBlock> CODEC = createCodec(settings -> new PokemonFarmBlock(FarmTier.IRON, settings));
+    public static final MapCodec<CobbleFarmBlock> CODEC = createCodec(settings -> new CobbleFarmBlock(FarmTier.IRON, settings));
+    private static final VoxelShape SHAPE = Block.createCuboidShape(1.0, 0.0, 1.0, 15.0, 14.0, 15.0);
 
     private final FarmTier tier;
 
-    public PokemonFarmBlock(FarmTier tier, AbstractBlock.Settings settings) {
+    public CobbleFarmBlock(FarmTier tier, AbstractBlock.Settings settings) {
         super(settings);
         this.tier = tier;
         this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
@@ -69,10 +72,15 @@ public class PokemonFarmBlock extends BlockWithEntity {
         return BlockRenderType.MODEL;
     }
 
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new PokemonFarmBlockEntity(pos, state, tier);
+        return new CobbleFarmBlockEntity(pos, state, tier);
     }
 
     @Override
@@ -90,7 +98,7 @@ public class PokemonFarmBlock extends BlockWithEntity {
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
         if (!state.isOf(newState.getBlock())) {
             BlockEntity be = world.getBlockEntity(pos);
-            if (be instanceof PokemonFarmBlockEntity farm) {
+            if (be instanceof CobbleFarmBlockEntity farm) {
                 farm.dropContents(world, pos);
             }
             super.onStateReplaced(state, world, pos, newState, moved);
@@ -101,7 +109,7 @@ public class PokemonFarmBlock extends BlockWithEntity {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         if (world.isClient) return null;
-        return validateTicker(type, com.cobblefarm.blockentity.CobbleFarmBlockEntities.POKEMON_FARM,
+        return validateTicker(type, com.cobblefarm.blockentity.CobbleFarmBlockEntities.COBBLEFARM,
                 (w, pos, s, be) -> be.tick(w, pos, s));
     }
 }
