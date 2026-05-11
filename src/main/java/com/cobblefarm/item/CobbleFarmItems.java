b4f8780852e06ea895da3be28d8734675e5412a6
Fagghino <fgiordano839@gmail.com>
2026-05-11 20:19:55 +0200

    feat: update CobbleFarmItems.java

--- src/main/java/com/cobblefarm/item/CobbleFarmItems.java
+++ src/main/java/com/cobblefarm/item/CobbleFarmItems.java
@@ -8,8 +8,6 @@ import net.minecraft.registry.Registry;
 public class CobbleFarmItems {
 
     public static final FarmBallItem FARM_BALL = register("farm_ball", new FarmBallItem());
-    public static final CapturedPokemonItem CAPTURED_POKEMON_ITEM =
-            register("captured_pokemon_item", new CapturedPokemonItem());
 
     private static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, CobbleFarm.id(name), item);
